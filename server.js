fdea076642eeed7570ded2b999d9158122d4b853
Wes <wmcmillan1012@gmail.com>
2020-10-08 18:49:59 -0700

    added some pseudo code and dependencies

---
+++ server.js
@@ -0,0 +1,12 @@
+//dependencies
+const express = require("express");
+const fs = require("fs");
+const notesArray = require("./db/db.json")
+// SETS UP THE EXPRESS APP 
+const app = express();
+const PORT = processd.env.PORT || 3000;
+// SETS UP THE EXPRESS APP TO HANDLE DATA PARSING 
+
+// STARTS THE SERVER TO BEGIN LISTENING 
+
+// ROUTES 
