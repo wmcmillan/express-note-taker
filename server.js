f0ed7158f818c95af6ff6a5776fe85e9dbf8aaf1
Wes <wmcmillan1012@gmail.com>
2020-10-16 16:50:30 -0700

    cleaned up some code

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 //dependencies
 const express = require("express");
 const fs = require("fs");
-const notesArray = require("./db/db.json")
+
 // SETS UP THE EXPRESS APP 
 const app = express();
-const PORT = processd.env.PORT || 3000;
+const PORT =  3000;
 // SETS UP THE EXPRESS APP TO HANDLE DATA PARSING 
 
 // STARTS THE SERVER TO BEGIN LISTENING 
