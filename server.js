f44f664c66ed5f8d934c8433aa906c26f94a305c
Wes <wmcmillan1012@gmail.com>
2020-10-18 13:37:56 -0700

    added pseudo code and cleaned up code. Also added server routing and app listener function

--- server.js
+++ server.js
@@ -2,15 +2,20 @@
 const express = require("express");
 const fs = require("fs");
 
+//express app
 const app = express();
-const port =  3000;
-const mainDir = path.join(__dirname, "/public")
+const PORT = process.env.PORT || 3000;
+app.use(express.static("public"))
 
-//express
-app.use(express.static("public"));
+//data parsing 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.get("/notes", function(req,res) {
-    Response.sendFile(path.join(mainDir, "notes.html"));
+//route to server
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+//listener
+app.listen(PORT, function() {
+    console.log("Listening on PORT: " + PORT);
 })
