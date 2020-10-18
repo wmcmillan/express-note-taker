37e88518762f9479f888e8ef780a5ea08fb3969f
Wes <wmcmillan1012@gmail.com>
2020-10-18 13:52:02 -0700

    addded html routes js file

---
+++ routes/html-routes.js
@@ -0,0 +1,13 @@
+//dependencies 
+const path = require("path");
+
+//export routing
+module.exports = function(app) {
+    app.get("/notes", function(req,res) {
+        res.sendFile(path.join(__dirname,"../public/notes.html"));
+    });
+
+    app.get("*", function(req,res) {
+        res.sendFile(path.join(__dirname,"../public/index.html"));
+    });
+}
